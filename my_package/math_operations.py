11ab2feb0bdbb4350d065fd4d9f24f576728a656
NT_005 <119782593+NT-005-TN@users.noreply.github.com>
2025-01-11 17:30:05 +0300

    Delete my_package directory

--- my_package/math_operations.py
+++
@@ -1,5 +0,0 @@
-# math_operations.py
-
-def multiply(a, b):
-    """Функция для умножения двух чисел."""
-    return a * b
--- my_package/string_operations.py
+++
@@ -1,5 +0,0 @@
-# string_operations.py
-
-def concatenate(str1, str2):
-    """Функция для конкатенации двух строк."""
-    return str1 + str2
